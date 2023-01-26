f8397bdec948e1746da7a0e3d2095691e12b657c
bigjawpp <118117125+bigjawpp@users.noreply.github.com>
2023-01-26 11:26:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import openai
+import telebot 
+
+openai.api_key = "YOUR_OPENAI_API"
+bot = telebot.TeleBot("YOUR_TELEGRAM_BOT_API")
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+    )
+    if response is not None:
+        if 'choices' in response and response['choices']:
+            bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+bot.polling()
